71db9c3b86649a098b1e7214274f7cf69c26ad1b
Echo Yin <echo_ybl@hotmail.com>
2024-01-03 19:14:10 -0500

    Add service locator bootstrapper

---
+++ Assets/ServiceLocator/Scripts/Bootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+namespace ServiceLocator.Scripts
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(ServiceLocator))]
+    public abstract class Bootstrapper : MonoBehaviour
+    {
+        private ServiceLocator _container;
+        internal ServiceLocator Container => _container.OrNull() ?? (_container = GetComponent<ServiceLocator>());
+
+        private bool _hasBeenBootstrapped;
+        private void Awake() => BootstrapOnDemand();
+
+        public void BootstrapOnDemand()
+        {
+            if (_hasBeenBootstrapped) return;
+            _hasBeenBootstrapped = true;
+            Bootstrap();
+        }
+
+        protected abstract void Bootstrap();
+    }
+
+    [AddComponentMenu("ServiceLocator/ServiceLocatorGlobal")]
+    public class ServiceLocatorGlobalBootstrapper : Bootstrapper
+    {
+        [SerializeField] private bool dontDestroyOnLoad = true;
+        protected override void Bootstrap()
+        {
+            // configuration for global
+        }
+    }
+
+    [AddComponentMenu("ServiceLocator/ServiceLocatorScene")]
+    public class ServiceLocatorSceneBootstrapper : Bootstrapper
+    {
+        protected override void Bootstrap()
+        {
+            // configuration for the scene
+            
+        }
+    }
+}
---
+++ Assets/ServiceLocator/Scripts/ServiceLocator.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ServiceLocator.Scripts
+{
+    public class ServiceLocator : MonoBehaviour
+    {
+        private static ServiceLocator _global;
+        private static Dictionary<Scene, ServiceLocator> _sceneContainers;
+
+        private readonly ServiceManager _services = new();
+
+        private const string GlobalServiceLocatorName = "ServiceLocatorGlobal";
+        private const string SceneServiceLocatorName = "ServiceLocatorScene";
+
+        public static ServiceLocator Global
+        {
+            get
+            {
+                if (_global != null) return _global;
+
+                if (FindFirstObjectByType<ServiceLocatorGlobalBootstrapper>() is { } found)
+                {
+                    found.BootstrapOnDemand();
+                    return _global;
+                }
+                
+                // bootstrap or initialize the new instance of global when non available
+                var container = new GameObject(GlobalServiceLocatorName, typeof(ServiceLocator));
+                container.AddComponent<ServiceLocatorGlobalBootstrapper>().BootstrapOnDemand();
+                return _global;
+            }
+        }
+
+        private static List<GameObject> _tempSceneGameObject;
+        
+        public static ServiceLocator ForSceneOf(MonoBehaviour monoBehaviour)
+        {
+            var scene = monoBehaviour.gameObject.scene;
+
+            // If the container is registered in the scene container
+            if (_sceneContainers.TryGetValue(scene, out var container) && container != monoBehaviour)
+            {
+                return container;
+            }
+            
+            // If not look through the root object of the scene and try to find one there.
+            _tempSceneGameObject.Clear();
+            scene.GetRootGameObjects(_tempSceneGameObject);
+
+            foreach (GameObject obj in _tempSceneGameObject.Where(obj =>
+                         obj.GetComponent<ServiceLocatorSceneBootstrapper>() != null))
+            {
+                if (obj.TryGetComponent(out ServiceLocatorSceneBootstrapper bootstrapper) &&
+                    bootstrapper.Container != monoBehaviour)
+                {
+                    bootstrapper.BootstrapOnDemand();
+                    return bootstrapper.Container;
+                }
+            }
+            
+            // If both cases above do not have any Scene Bootstrapper, simply return global one.
+            return Global;
+        }
+    }
+}
